0a6853febf4d933427bf4fe8636dd18d4c620ae8
Bakhtovar Gulomov <gulomov@live.ru>
2023-03-20 14:39:24 +0700

    Added plugin vehicle

--- _build/build.php
+++ _build/build.php
@@ -170,6 +170,68 @@ class FetchItPackage
 
 
     /**
+     * Add plugins
+     */
+    protected function plugins()
+    {
+        /** @noinspection PhpIncludeInspection */
+        $plugins = include($this->config['elements'] . 'plugins.php');
+        if (!is_array($plugins)) {
+            $this->modx->log(modX::LOG_LEVEL_ERROR, 'Could not package in Plugins');
+
+            return;
+        }
+        $this->category_attributes[xPDOTransport::RELATED_OBJECT_ATTRIBUTES]['Plugins'] = [
+            xPDOTransport::UNIQUE_KEY => 'name',
+            xPDOTransport::PRESERVE_KEYS => false,
+            xPDOTransport::UPDATE_OBJECT => !empty($this->config['update']['plugins']),
+            xPDOTransport::RELATED_OBJECTS => true,
+            xPDOTransport::RELATED_OBJECT_ATTRIBUTES => [
+                'PluginEvents' => [
+                    xPDOTransport::PRESERVE_KEYS => true,
+                    xPDOTransport::UPDATE_OBJECT => true,
+                    xPDOTransport::UNIQUE_KEY => ['pluginid', 'event'],
+                ],
+            ],
+        ];
+        $objects = [];
+        foreach ($plugins as $name => $data) {
+            /** @var modPlugin $plugin */
+            $plugin = $this->modx->newObject('modPlugin');
+            $plugin->fromArray(array_merge([
+                'name' => $name,
+                'category' => 0,
+                'description' => @$data['description'],
+                'plugincode' => $this::_getContent($this->config['core'] . 'elements/plugins/' . $data['file'] . '.php'),
+                'static' => !empty($this->config['static']['plugins']),
+                'source' => 1,
+                'static_file' => 'core/components/' . $this->config['name_lower'] . '/elements/plugins/' . $data['file'] . '.php',
+            ], $data), '', true, true);
+
+            $events = [];
+            if (!empty($data['events'])) {
+                foreach ($data['events'] as $event_name => $event_data) {
+                    /** @var modPluginEvent $event */
+                    $event = $this->modx->newObject('modPluginEvent');
+                    $event->fromArray(array_merge([
+                        'event' => $event_name,
+                        'priority' => 0,
+                        'propertyset' => 0,
+                    ], $event_data), '', true, true);
+                    $events[] = $event;
+                }
+            }
+            if (!empty($events)) {
+                $plugin->addMany($events);
+            }
+            $objects[] = $plugin;
+        }
+        $this->category->addMany($objects);
+        $this->modx->log(modX::LOG_LEVEL_INFO, 'Packaged in ' . count($objects) . ' Plugins');
+    }
+
+
+    /**
      * Add snippets
      */
     protected function snippets()
