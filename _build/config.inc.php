c89c7feea634f445b621ab7396ec04dd206eb1c4
Bakhtovar Gulomov <gulomov@live.ru>
2023-11-11 15:04:56 +0700

    Release 1.1.2

--- _build/config.inc.php
+++ _build/config.inc.php
@@ -11,7 +11,7 @@ if (!defined('MODX_CORE_PATH')) {
 return [
     'name' => 'FetchIt',
     'name_lower' => 'fetchit',
-    'version' => '1.1.1',
+    'version' => '1.1.2',
     'release' => 'pl',
     // Install package to site right after build
     'install' => true,
--- core/components/fetchit/model/fetchit.class.php
+++ core/components/fetchit/model/fetchit.class.php
@@ -2,7 +2,7 @@
 
 class FetchIt
 {
-    public $version = '1.1.1';
+    public $version = '1.1.2';
     /** @var modX $modx */
     public $modx;
     /** @var array $config */
